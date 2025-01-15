d3edf116f979a2bb95e6c0efd68fca1945e778ea
paivim <paivsmehraj12@gmail.com>
2025-01-15 20:49:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -179,6 +179,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Tables created successfully!")
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 
