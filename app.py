9fe120eaf60732e58be100601e8b52a46d2be49f
paivim <paivsmehraj12@gmail.com>
2025-01-15 18:44:46 +0000

    Updated app.py to run on 0.0.0.0:5001

--- app.py
+++ app.py
@@ -179,4 +179,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Tables created successfully!")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
+
+
